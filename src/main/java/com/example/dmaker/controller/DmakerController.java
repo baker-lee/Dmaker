3e6259842b73aec19fc763dd9033e7a78a0ad49d
baker-lee <shishi1340@gmail.com>
2022-12-28 15:34:38 +0900

    feat: 예외처리 Validation 설정

--- src/main/java/com/example/dmaker/controller/DmakerController.java
+++ src/main/java/com/example/dmaker/controller/DmakerController.java
@@ -1,12 +1,17 @@
 package com.example.dmaker.controller;
 
+import com.example.dmaker.dto.CreateDeveloper;
 import com.example.dmaker.service.DmakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -22,11 +27,14 @@ public class DmakerController {
         return Arrays.asList("snow", "elsa", "olaf");
     }
 
-    @GetMapping("/create-developers")
-    public List<String> createDevelopers() {
-        dmakerService.createDeveloper();
+    @PostMapping("/create-developers")
+    public List<String> createDevelopers(
+            @Valid @RequestBody CreateDeveloper.Request request
+            ) {
+        log.info("request : " + request);
+        dmakerService.createDeveloper(request);
 
-        return List.of("Olaf");
+        return Collections.singletonList("Olaf");
     }
 
 }
--- src/main/java/com/example/dmaker/service/DmakerService.java
+++ src/main/java/com/example/dmaker/service/DmakerService.java
@@ -1,6 +1,9 @@
 package com.example.dmaker.service;
 
+import com.example.dmaker.dto.CreateDeveloper;
 import com.example.dmaker.entity.Developers;
+import com.example.dmaker.exception.DMakerErrorCode;
+import com.example.dmaker.exception.DMakerException;
 import com.example.dmaker.repository.DeveloperRepository;
 import com.example.dmaker.type.DeveloperLevel;
 import com.example.dmaker.type.DeveloperSkillType;
@@ -8,13 +11,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
+import java.util.Optional;
+
+import static com.example.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.example.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class DmakerService {
     private final DeveloperRepository developerRepository;
+    private final EntityManager em;
 
     @Transactional
-    public void createDeveloper(){
+    public void createDeveloper(CreateDeveloper.Request request) {
         Developers developers = Developers.builder()
                 .developerLevel(DeveloperLevel.JUNIOR)
                 .developerSkillType(DeveloperSkillType.FRONT_END)
@@ -24,5 +36,27 @@ public class DmakerService {
                 .build();
 
         developerRepository.save(developers);
+    }
+
+    private void validateCreatDeveloperRequest(CreateDeveloper.Request request) {
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR
+                && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+                && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNGNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        Optional<Developers> developer = developerRepository.findByMemberId(request.getMemberId());
+        if (developer.isPresent())
+            throw new DMakerException((DUPLICATED_MEMBER_ID));
     }
 }
